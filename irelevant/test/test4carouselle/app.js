b4997b3577fe3990d694d61c5cde44de73ada575
Cristian-Mihail Dumitrescu <dumi_cristi@yahoo.com>
2020-07-03 00:05:35 +0300

    try to remake carousel in 3D test3

---
+++ irelevant/test/test4carouselle/app.js
@@ -0,0 +1,101 @@
+const carouselImages = document.querySelectorAll("img");
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let counter = 0;
+var i;
+
+nextBtn.addEventListener("click", () => {
+  switch (counter) {
+    case 6:
+      counter = 0;
+      carouselImages[counter + 1].style.left = "25vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[counter + 1].style.transition = "left 0.4s ease-in-out";
+      carouselImages[counter + 1].style.opacity = "1";
+      carouselImages[counter].style.opacity = "0";
+      carouselImages[counter + 2].style.left = "75vw";
+      carouselImages[counter].style.left = "-25vw";
+      break;
+    case 5:
+      carouselImages[0].style.left = "25vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[0].style.transition = "left 0.4s ease-in-out";
+      carouselImages[0].style.opacity = "1";
+      carouselImages[counter].style.opacity = "0";
+      carouselImages[1].style.left = "75vw";
+      carouselImages[counter].style.left = "-25vw";
+      break;
+    case 4:
+      carouselImages[0].style.left = "75vw";
+      carouselImages[counter + 1].style.left = "25vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[counter + 1].style.transition = "left 0.4s ease-in-out";
+      carouselImages[counter + 1].style.opacity = "1";
+      carouselImages[counter].style.opacity = "0";
+      carouselImages[counter].style.left = "-25vw";
+      break;
+    default:
+      carouselImages[counter + 1].style.left = "25vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[counter + 1].style.transition = "left 0.4s ease-in-out";
+      carouselImages[counter + 1].style.opacity = "1";
+      carouselImages[counter].style.opacity = "0";
+      carouselImages[counter + 2].style.left = "75vw";
+      carouselImages[counter].style.left = "-25vw";
+      break;
+  }
+
+  for (i = counter + 1; i <= 5; i++) {
+    carouselImages[i].style.zIndex = carouselImages.length + 1 - i + counter;
+  }
+  for (i = 0; i < counter; i++) {
+    carouselImages[i].style.zIndex = carouselImages.length - 5 - i + counter;
+  }
+
+  counter++;
+  console.log(counter);
+});
+prevBtn.addEventListener("click", () => {
+  switch (counter) {
+    case 6:
+      carouselImages[5].style.left = "25vw";
+      carouselImages[5].style.transition = "left 0.4s ease-in-out";
+      carouselImages[5].style.opacity = "1";
+      carouselImages[0].style.opacity = "0";
+      carouselImages[4].style.left = "-25vw";
+      carouselImages[0].style.left = "75vw";
+      carouselImages[5].style.zIndex = "6";
+      break;
+    case 1:
+      counter = 7;
+      carouselImages[0].style.left = "25vw";
+      carouselImages[0].style.transition = "left 0.4s ease-in-out";
+      carouselImages[0].style.opacity = "1";
+      carouselImages[1].style.opacity = "0";
+      carouselImages[5].style.left = "-25vw";
+      carouselImages[1].style.left = "75vw";
+      carouselImages[0].style.zIndex = "6";
+      break;
+    default:
+      carouselImages[counter - 1].style.left = "25vw";
+      carouselImages[counter - 1].style.transition = "left 0.4s ease-in-out";
+      carouselImages[counter - 1].style.opacity = "1";
+      carouselImages[counter].style.opacity = "0";
+      carouselImages[counter - 2].style.left = "-25vw";
+      carouselImages[counter].style.left = "75vw";
+      carouselImages[counter - 1].style.zIndex = "6";
+      break;
+  }
+
+  for (i = 0; i < counter - 1; i++) {
+    carouselImages[i].style.zIndex = counter - i - 1;
+  }
+  for (i = counter; i <= 5; i++) {
+    carouselImages[i].style.zIndex = counter - i + 5;
+  }
+
+  counter--;
+  console.log(counter);
+});
---
+++ irelevant/test/test4carouselle/myFunction.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  document.getElementById("img1").style.zIndex = "-4;";
+  counter++;
+}
