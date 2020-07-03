4e47dfa778aa693ae6c7635a784073a63fd700dd
Cristian-Mihail Dumitrescu <dumi_cristi@yahoo.com>
2020-07-03 16:12:37 +0300

    final responsive carousel

--- js/app.js
+++ js/app.js
@@ -1,36 +1,157 @@
-const carouselSlide = document.querySelector(".carousel-slide");
-const carouselImages = document.querySelectorAll(".carousel-slide .display");
+const carouselImages = document.querySelectorAll(".display");
 
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
-let counter = 1;
-const size = carouselImages[0].clientWidth;
+let counter = 0;
+var i;
 
-carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+carouselImages[counter].style.left = "-50vw";
+carouselImages[counter].style.left = "0vw";
 
 nextBtn.addEventListener("click", () => {
-  if (counter >= carouselImages.length - 1) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
+  switch (counter) {
+    case 6:
+      counter = 0;
+      carouselImages[counter + 1].style.left = "0vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[counter + 1].style.opacity = "1";
+      carouselImages[counter + 2].style.left = "50vw";
+      carouselImages[counter].style.left = "-50vw";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[counter - 1].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+
+      break;
+    case 5:
+      carouselImages[0].style.left = "0vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[0].style.opacity = "1";
+      carouselImages[1].style.left = "50vw";
+      carouselImages[counter].style.left = "-50vw";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[counter - 1].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
+    case 4:
+      carouselImages[0].style.left = "50vw";
+      carouselImages[counter + 1].style.left = "0vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[counter + 1].style.opacity = "1";
+      carouselImages[counter].style.left = "-50vw";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[counter - 1].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
+    default:
+      carouselImages[counter + 1].style.left = "0vw";
+      carouselImages[counter].style.zIndex = "1";
+      carouselImages[counter + 1].style.opacity = "1";
+      carouselImages[counter + 2].style.left = "50vw";
+      carouselImages[counter].style.left = "-50vw";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[counter - 1].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
+  }
+
+  for (i = counter + 1; i <= 5; i++) {
+    carouselImages[i].style.zIndex = carouselImages.length + 1 - i + counter;
+  }
+  for (i = 0; i < counter; i++) {
+    carouselImages[i].style.zIndex = carouselImages.length - 5 - i + counter;
+  }
+
   counter++;
-  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+  console.log(counter);
 });
 prevBtn.addEventListener("click", () => {
-  if (counter <= 0) return;
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-});
+  switch (counter) {
+    case 6:
+      carouselImages[5].style.left = "0vw";
+      carouselImages[5].style.opacity = "1";
 
-carouselSlide.addEventListener("transitionend", () => {
-  if (carouselImages[counter].id === "lastclone") {
-    carouselSlide.style.transition = "none";
-    counter = carouselImages.length - 2;
-    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+      carouselImages[4].style.left = "-50vw";
+      carouselImages[0].style.left = "50vw";
+      carouselImages[5].style.zIndex = "6";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[0].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
+    case 0:
+      counter = 6;
+      carouselImages[5].style.left = "0vw";
+      carouselImages[5].style.opacity = "1";
+
+      carouselImages[4].style.left = "-50vw";
+      carouselImages[0].style.left = "50vw";
+      carouselImages[5].style.zIndex = "6";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[0].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
+    case 1:
+      counter = 7;
+      carouselImages[0].style.left = "0vw";
+      carouselImages[0].style.opacity = "1";
+      carouselImages[5].style.left = "-50vw";
+      carouselImages[1].style.left = "50vw";
+      carouselImages[0].style.zIndex = "6";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[1].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
+    default:
+      carouselImages[counter - 1].style.left = "0vw";
+      carouselImages[counter - 1].style.opacity = "1";
+      carouselImages[counter - 2].style.left = "-50vw";
+      carouselImages[counter].style.left = "50vw";
+      carouselImages[counter - 1].style.zIndex = "6";
+      nextBtn.disabled = true;
+      prevBtn.disabled = true;
+      setTimeout(function () {
+        carouselImages[counter + 1].style.opacity = "0";
+        nextBtn.disabled = false;
+        prevBtn.disabled = false;
+      }, 400);
+      break;
   }
-  if (carouselImages[counter].id === "firstclone") {
-    carouselSlide.style.transition = "none";
-    counter = carouselImages.length - counter;
-    carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
+
+  for (i = 0; i < counter - 1; i++) {
+    carouselImages[i].style.zIndex = counter - i - 1;
   }
+  for (i = counter; i <= 5; i++) {
+    carouselImages[i].style.zIndex = counter - i + 5;
+  }
+
+  counter--;
+  console.log(counter);
 });
