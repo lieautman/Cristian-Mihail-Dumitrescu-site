d4be6932a7b8bce3ec10e390341315259bd08bb5
Cristian-Mihail Dumitrescu <dumi_cristi@yahoo.com>
2020-07-05 01:35:27 +0300

    nav final

--- js/app.js
+++ js/app.js
@@ -155,3 +155,28 @@ prevBtn.addEventListener("click", () => {
   counter--;
   console.log(counter);
 });
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navlist");
+  const navLinks = document.querySelectorAll(".navlist li");
+
+  burger.addEventListener("click", () => {
+    //Toggle nav
+    nav.classList.toggle("nav-active");
+
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    //burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
