b2d4a1b2182dfb917d5631160619ce7f3bcc9350
Cristian-Mihail Dumitrescu <dumi_cristi@yahoo.com>
2020-07-05 12:12:05 +0300

    nav bugfix8+footer

--- js/app.js
+++ js/app.js
@@ -167,10 +167,8 @@ const navSlide = () => {
     //animate links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
-        $(".nav").hide();
         link.style.animation = "";
       } else {
-        $(".nav").show();
         link.style.animation = `navLinkFade 0.5s ease forwards ${
           index / 7 + 0.5
         }s`;
